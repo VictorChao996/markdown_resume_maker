ad875eca40f4a97bf589325bbf8f5f0075c5f2cf
VictorChao996 <96094victor@gmail.com>
2023-05-12 22:00:46 +0800

    setup: add frontend & package

---
+++ README.md
@@ -0,0 +1,5 @@
+## npm package used
+- pdfkit
+- antd
+- showdown
+- express
---
+++ db.sql
@@ -0,0 +1,9 @@
+--@block
+-- CREATE TABLE users(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(16) NOT NULL,
+--     email VARCHAR(32) NOT NULL,
+--     PRIMARY KEY (id)
+-- ); 
+
+
