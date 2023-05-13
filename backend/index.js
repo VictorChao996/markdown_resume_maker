07a8f7d19f7d43715f98bd69c38ed876dfb93fbc
VictorChao996 <96094victor@gmail.com>
2023-05-13 13:32:23 +0800

    CICD test #1
    CICD test #2
    
    CICD test #3
    
    CICD test #4
    
    CICD test #5
    
    CICD test #6
    
    CICD test #7

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3003
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+app.get("/api/", (req, res) => {
+    res.send("backend: Hello World")
+})
+
+app.get("/api/healthCheck", (req, res) => {
+    res.send("OK")
 })
 
 app.listen(port, () => {
