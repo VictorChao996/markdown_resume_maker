9939c57a26cff384ae750d169b8271016aad37e7
VictorChao996 <96094victor@gmail.com>
2023-05-16 13:42:55 +0800

    refector: test frontened .env file

--- frontend/src/utils/API.js
+++ frontend/src/utils/API.js
@@ -1,7 +1,10 @@
-const url = "http://54.64.217.57/"
+// const url = "http://54.64.217.57/"
 // const url = "http://localhost:3003"
+const url = process.env.REACT_APP_API_URL
 
-export default {
+const apiUrls = {
     signInAPI: `${url}/api/user/signin`,
     signUpAPI: `${url}/api/user/signup`
 }
+
+export default apiUrls
